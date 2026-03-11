2a15da4b6a90ca32a8f5635b0029cc4f7df04420
yashrmalhotra <yashrmalhotra11@gmail.com>
2026-03-11 16:31:14 +0530

    debugging

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode:false,
   async rewrites(){
-    
+    console.log("next rewrite",process.env.NEXT_PUBLIC_SERVER_URL)
     return [
       {
         source:"/api/:path*",
