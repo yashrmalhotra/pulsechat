1b38720499d61f5fc1a383b29b2aa759609d19ec
yashrmalhotra <yashrmalhotra11@gmail.com>
2026-03-11 18:09:50 +0530

    testing rewrites

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,14 +1,15 @@
 import type { NextConfig } from "next";
+console.log("SERVER_URL:", process.env.NEXT_PUBLIC_SERVER_URL)
 const nextConfig: NextConfig = {
   reactStrictMode:false,
-  // async rewrites(){
-  //   return [
-  //     {
-  //       source:"/api/:path*",
-  //       destination:`${process.env.NEXT_PUBLIC_SERVER_URL}/api/:path*`
-  //     }
-  //   ]
-  // }
+  async rewrites(){
+    return [
+      {
+        source:"/api/:path*",
+        destination:`${process.env.NEXT_PUBLIC_SERVER_URL}/api/:path*`
+      }
+    ]
+  }
 };
 
 export default nextConfig;
